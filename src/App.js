64a00854dd351e1e496ca099dc23dcc34a8a459e
Nihar Singla <nihar.singla@mettl.com>
2021-01-21 15:56:16 +0530

    react hooks useEffect

--- src/App.js
+++ src/App.js
@@ -1,23 +1,66 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React, { useEffect, useState } from "react";
+import { useForm } from "./useForm";
+import { Hello } from "./Hello";
+import { useFetch } from "./useFetch";
 
 function App() {
+  const [values, handleChange] = useForm({
+    email: "",
+    password: "",
+    firstName: "",
+  });
+
+  // useEffect(() => {
+  //   const onMouseMove = (e) => {
+  //     console.log(e);
+  //   };
+  //   window.addEventListener("mousemove", onMouseMove);
+
+  //   return () => {
+  //     window.removeEventListener("mousemove", onMouseMove);
+  //   };
+  // }, []);
+
+  // useEffect(() => {
+  //   console.log("mount 1");
+  // });
+
+  // useEffect(() => {
+  //   console.log("mount 2");
+  // });
+  // const [showHello, setShowHello] = useState(true);
+
+  const [count, setCount] = useState(() =>
+    JSON.parse(localStorage.getItem("count"))
+  );
+
+  const { data, loading } = useFetch(`http://numbersapi.com/${count}/trivia`);
+
+  useEffect(() => {
+    localStorage.setItem("count", JSON.stringify(count));
+  }, [count]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* {showHello && <Hello />}
+      <button onClick={() => setShowHello(!showHello)}>Toggle Hello</button> */}
+      <div>{!data ? "Loading..." : data}</div>
+      <div>count: {count}</div>
+      <button onClick={() => setCount((c) => c + 1)}>Increment</button>
+      <input
+        name="firstName"
+        value={values.firstName}
+        onChange={handleChange}
+        placeholder="First Name"
+      ></input>
+      <input name="email" value={values.email} onChange={handleChange}></input>
+      <input
+        type="password"
+        name="password"
+        value={values.password}
+        onChange={handleChange}
+      ></input>
     </div>
   );
 }
